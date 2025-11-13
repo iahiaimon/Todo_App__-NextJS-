d6f026a3131bca42f6fdf65845b107fc8b4de246
Iahia Imon <iahiaimon555@gmail.com>
2025-11-14 03:23:28 +0600

    checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="max-w-4xl mx-auto mt-5">
       <div className="text-center my-center flex flex-col gap-5">
         <h1 className="text-3xl">Todo List App</h1>
+        <h3>The todo app is now updated</h3>
         <AddTask />
         <TodoList />
       </div>
